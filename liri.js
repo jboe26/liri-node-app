0098a3f9848cd724c0bf2c5532f928dc543cd110
jboe26 <joshrboepple@outlook.com>
2019-09-23 18:27:00 -0400

    finished the movie function

--- liri.js
+++ liri.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 // Use node inquirer to get user input
-var inquirer = require('inquirer');
+// var inquirer = require('inquirer');
 // Include the axios npm package (Don't forget to run "npm install axios" in this folder first!)
 var axios = require("axios");
 // Includes the FS package for reading and writing packages
@@ -14,46 +14,48 @@ var Spotify = require('node-spotify-api');
 var omdb = require('omdb');
 
 
-// prompt the user to select an option from the list
-inquirer.prompt([
+// // prompt the user to select an option from the list
+// inquirer.prompt([
 
-    {
-        type: "list",
-        name: "commands",
-        message: "Make a selection for LIRI to do!",
-        choices: ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"]
-    },
+//     {
+//         type: "list",
+//         name: "commands",
+//         message: "Make a selection for LIRI to do!",
+//         choices: ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"]
+//     },
 
-    {
-        type: "input",
-        name: "userInput",
-        message: "What artist/band would you like?"
-    }
+//     {
+//         type: "input",
+//         name: "userInput",
+//         message: "What artist/band would you like?"
+//     }
 
     // After the prompt, store the user's response in a variable called location.
 
-])
-    .then(function (inquirerResponse) {
-        console.log(inquirerResponse) 
-    })
+// ])
+    // .then(function (inquirerResponse) {
+    //     console.log(inquirerResponse) 
+    // })
 
-function userPrompt(command, userSearch) {
+    var command = process.argv[2];
+    var search =  process.argv[3];
+
     // make a decision based on the command
     switch (command) {
         case "concert-this":
-            concertThis();
+            concertThis(search);
             break;
         case "spotify-this":
-            spotifyThisSong();
+            spotifyThisSong(search);
             break;
         case "movie-this":
-            movieThis();
+            movieThis(search);
             break;
         case "do-what-it-says":
-            doThis();
+            doThis(search);
             break;
     }
-}
+
 
 // Running the readFile module that's inside of fs.
 // Stores the read information into the variable "data"
@@ -76,8 +78,8 @@ fs.readFile("random.txt", "utf8", function(err, data) {
 
 function concertThis() {
 
-    var artist = 
-
+    var artist = process.argv[2];
+    console.log(artist);
 
     // Then run a request with axios to the OMDB API with the movie specified
     axios.get(("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"))
@@ -113,12 +115,12 @@ function spotifyThisSong() {
 
 var spotify = new Spotify(keys.spotify);
  
-var spotify = new Spotify({
-  id: "4d227c9d03e444359cd1ae63898cb027",
-  secret: "a20503eecb1741c3938ea49a95259f6e"
-});
+// var Spotify = new Spotify({
+//   id: "4d227c9d03e444359cd1ae63898cb027",
+//   secret: "a20503eecb1741c3938ea49a95259f6e"
+// });
  
-spotify.search({ type: 'track', query: 'spotify-this-song' }, function(err, data) {
+spotify.search({ type: 'track', query: 'The Sign' }, function(err, data) {
   if (err) {
     return console.log('Error occurred: ' + err);
   }
@@ -141,35 +143,44 @@ spotify
 
 
 function movieThis() {
-    omdb.search('Mr. Nobody', function(err, movies) {
-        if(err) {
-            return console.error(err);
-        }
-     
-        if(movies.length < 1) {
-            return console.log('No movies were found!');
-        }
-     
-        movies.forEach(function(movie) {
-            console.log(movie.title, movie.year);
-        });
-     
-    });
-     
-    omdb.get({ title: 'Mr. Nobody'}, true, function(err, movie) {
-        if(err) {
-            return console.error(err);
-        }
-     
-        if(!movie) {
-            return console.log('Movie not found!');
-        }
-     
-        console.log(movie.title, movie.year, movie.imdb.rating);
-        console.log(movie.plot);
-     
-        
-    });
+
+// Then run a request with axios to the OMDB API with the movie specified
+axios.get("http://www.omdbapi.com/?t="+search+"&y=&plot=short&apikey=trilogy")
+  .then(
+    function (response) {
+   console.log("+++++++++++++++++++++++++++++++++++++++++++++++");
+   console.log("Title: " + response.data.Title);
+   console.log("Year: " + response.data.Year);
+   console.log("IMDB Rating: " + response.data.imdbRating);
+   console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1]);
+   console.log("Country of origin: " + response.data.Country);
+   console.log("Language: " + response.data.Language);
+   console.log("Plot: " + response.data.Plot);
+   console.log("Actors/Actresses: " + response.data.Actors);
+   console.log("+++++++++++++++++++++++++++++++++++++++++++++++");
+    }
+  )
+  .catch(function (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  });
+
     
     }
 
